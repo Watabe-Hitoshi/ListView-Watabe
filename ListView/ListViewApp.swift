b4d0c6baee47a4cf54df9f2c485d5302d536f420
Hitoshi <hitoshiwtb1104@gmail.com>
2025-02-09 14:36:33 +0900

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by hitoshi on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
